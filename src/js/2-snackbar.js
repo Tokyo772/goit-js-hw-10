3d31e8fa4a6544229f318dc5e2258820e2b1a663
Tokyo772 <merokyry33@gmail.com>
2024-04-16 16:29:26 +0200

    task 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,20 +3,64 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const elements = {
   form: document.querySelector('.form'),
-  input: document.querySelector('[name="delay"]'),
-  label: document.querySelector('[name="state"]'),
+  input: document.querySelector('input[name="delay"]'),
+  btnSubmit: document.querySelector('button'),
 };
 
-elements.form.addEventListener('input', handlerInput);
+const iziSuccessOptions = {
+  title: '✅',
+  titleSize: '24px',
+  messageColor: 'white',
+  messageSize: '16px',
+  backgroundColor: 'lightgreen',
+  position: 'topRight',
+  timeout: 3000,
+};
+const iziRejectOptions = {
+  title: '❌',
+  titleSize: '24px',
+  messageColor: 'white',
+  messageSize: '16px',
+  backgroundColor: 'rgba(225, 0, 0, 0.3)',
+  position: 'topRight',
+  timeout: 3000,
+};
 elements.form.addEventListener('submit', handlerSubmit);
-
-function handlerInput(evt) {
-  const choseNumber = elements.input.value;
-  const choseStatus = document.querySelector('[name="state"]:checked')?.value;
-  console.log(choseNumber);
-  console.log(choseStatus);
-}
 
 function handlerSubmit(evt) {
   evt.preventDefault();
+  const { delay, state } = evt.target.elements;
+  const selectedDelay = delay.value;
+  const selectedStatus = state.value;
+
+  createPromise(selectedDelay, selectedStatus)
+    .then(value => {
+      return value;
+    })
+    .catch(err => {
+      return err;
+    });
+  elements.form.reset();
+}
+
+function createPromise(selectedDelay, selectedStatus) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      if (selectedStatus === 'fulfilled') {
+        res(
+          iziToast.show({
+            ...iziSuccessOptions,
+            message: `Fulfilled promise in ${selectedDelay}ms`,
+          })
+        );
+      } else {
+        rej(
+          iziToast.show({
+            ...iziRejectOptions,
+            message: `Rejected promise in ${selectedDelay}ms`,
+          })
+        );
+      }
+    }, selectedDelay * 1000);
+  });
 }
